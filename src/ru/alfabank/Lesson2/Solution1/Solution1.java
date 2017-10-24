4bde1ad15cf1af928ac86aa2505aba00f1fa1a1d
evgeniy_yakovenko <him_2po@mail.ru>
2017-10-24 10:38:06 +0300

    first commit

--- src/ru/alfabank/Lesson2/Solution1/Solution1.java
+++ src/ru/alfabank/Lesson2/Solution1/Solution1.java
@@ -2,7 +2,7 @@ package ru.alfabank.Lesson2.Solution1;
 
 public class Solution1 {
     public static void main(String[] args) {
-        String Str = new String("Рассерженный Родион Романович раскричался: «Рано радовались, размокший ремень рюкзака разорвался");
-        System.out.println(Str.replace('р', '*'));
+        String str = new String("Рассерженный Родион Романович раскричался: «Рано радовались, размокший ремень рюкзака разорвался");
+        System.out.println(str.replace('р', '*'));
     }
 }
--- src/ru/alfabank/Lesson2/Solution4/Solution4.java
+++ src/ru/alfabank/Lesson2/Solution4/Solution4.java
@@ -1,5 +1,9 @@
 package ru.alfabank.Lesson2.Solution4;
 
 public class Solution4 {
-
+    public static void main(String[] args) {
+        String s = new String("Я у мамы программист");
+        System.out.println(s.toLowerCase());
+        System.out.println(s.toCharArray());
+    }
 }
